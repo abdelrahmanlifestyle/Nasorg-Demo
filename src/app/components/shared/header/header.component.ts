18bd8aa29992dbb381123b74cd213de3638c51d0
abdelrahman.lifestyle@gmail.com <abdelrahman.lifestyle@gmail.com>
2018-09-30 14:56:29 +0200

    adding mobile design + toggle button animated + fix responsive bugs

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+    open: boolean;
+    constructor() { }
 
-  constructor() { }
+    ngOnInit() {
+        this.open = false;
+    }
 
-  ngOnInit() {
-  }
+    onToggleClick() {
+        this.open = !this.open;
+    }
 
 }
